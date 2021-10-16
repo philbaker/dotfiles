2ae8eea88bf834531486b52379a48eda01749ab5
philbaker <phil@philbaker.me>
2021-10-16 22:43:56 +0100

    Add helpful

--- emacs/init.el
+++ emacs/init.el
@@ -447,3 +447,15 @@
 (use-package treemacs-projectile
   :after treemacs projectile)
 
+;;
+;; Help
+;;
+
+(use-package helpful)
+
+(global-set-key (kbd "C-h f") #'helpful-callable)
+(global-set-key (kbd "C-h v") #'helpful-variable)
+(global-set-key (kbd "C-h k") #'helpful-key)
+(global-set-key (kbd "C-c C-d") #'helpful-at-point)
+(global-set-key (kbd "C-h F") #'helpful-function)
+(global-set-key (kbd "C-h C") #'helpful-command)
