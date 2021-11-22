5af746c4d7d132c684b6daca692b2e727cb107eb
philbaker <philbaker@users.noreply.github.com>
2021-11-22 18:44:15 +0000

    Remove sounds

--- emacs/init.el
+++ emacs/init.el
@@ -633,7 +633,8 @@
 
 (use-package org-pomodoro)
 (setq org-pomodoro-manual-break t
-  org-pomodoro-keep-killed-pomodoro-time t)
+  org-pomodoro-keep-killed-pomodoro-time t
+  org-pomodoro-play-sounds nil)
 
 (org-babel-do-load-languages
   'org-babel-load-languages
