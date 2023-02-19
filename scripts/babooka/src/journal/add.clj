bce50f80dc0ab4bd769919a6da5cf4b3ba144363
philbaker <phil@philbaker.me>
2023-02-19 13:14:14 +0000

    add babooka bb examples

---
+++ scripts/babooka/src/journal/add.clj
@@ -0,0 +1,9 @@
+(ns journal.add
+  (:require [journal.utils :as utils]))
+
+(defn add-entry [opts]
+  (let [entries (utils/read-entries)]
+    (spit utils/ENTRIES-LOCATION
+          (conj entries
+                (merge {:timestamp (System/currentTimeMillis)}
+                       opts)))))
---
+++ scripts/babooka/src/journal/list.clj
@@ -0,0 +1,8 @@
+(ns journal.list 
+  (:require [journal.utils :as utils]))
+
+(defn list-entries [_]
+  (let [entries (utils/read-entries)]
+    (doseq [{:keys [timestamp entry]} (reverse entries)]
+      (println timestamp)
+      (println entry "\n"))))
