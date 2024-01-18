956a3c1c2d3331f4ddd090d39a2798abbfa5a3ac
philbaker <phil@philbaker.me>
2024-01-18 20:59:52 +0000

    update settings

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -4,10 +4,17 @@ local autoload = _local_1_["autoload"]
 local nvim = autoload("nvim")
 local core = autoload("nfnl.core")
 nvim.set_keymap("n", "<space>", "<nop>", {noremap = true})
+nvim.ex.set("nowrap")
 do
-  local options = {expandtab = true, tabstop = 2, shiftwidth = 2, softtabstop = 2, completeopt = "menuone,noselect", ignorecase = true, smartcase = true, clipboard = "unnamedplus", number = true, ruler = true, signcolumn = "number"}
+  local options = {backup = true, termguicolors = true, colorcolumn = "80", splitbelow = true, splitright = true, completeopt = "menuone,noselect", inccommand = "nosplit", ignorecase = true, smartcase = true, signcolumn = "yes", undofile = true, undodir = vim.fn.expand("~/.undodir"), confirm = true, breakindent = true, list = true, clipboard = "unnamedplus", scrolloff = 3, number = true, relativenumber = true, swapfile = false}
   for option, value in pairs(options) do
     core.assoc(nvim.o, option, value)
   end
 end
+nvim.ex.autocmd("FileType", "*", "setlocal formatoptions-=c formatoptions-=r formatoptions-=o")
+nvim.ex.let("neoterm_autoinsert=1")
+nvim.ex.autocmd("FileType", "qf", "wincmd J")
+vim.cmd("autocmd BufNewFile,BufRead *.phel set syntax=clojure")
+do end (vim.opt.backupdir):remove(".")
+vim.opt.listchars = {tab = "\226\150\184 ", trail = "\194\183"}
 return {}
