49ce0005a23c7e0340170059f363c649fd8341d0
philbaker <philbaker@users.noreply.github.com>
2024-05-24 16:09:16 +0100

    org mode and telescope fixes

--- nvim/lua/config/scratch.lua
+++
@@ -1,6 +0,0 @@
--- [nfnl] Compiled from fnl/config/scratch.fnl by https://github.com/Olical/nfnl, do not edit.
-local _local_1_ = require("nfnl.module")
-local autoload = _local_1_["autoload"]
-local util = autoload("config.util")
---[[ (os.clock) (os.getenv "HOME") (vim.print "hello") (vim.fn.getline 11) (vim.cmd (vim.fn.input "echo hi")) (vim.fn.setqflist {} " " {:lines (vim.fn.systemlist "ls -la")}) (vim.fn.setqflist {} " " {:lines ["hello" "world"]}) (vim.cmd.copen) (vim.cmd.cclose) (util.tailwind-color "ch" "red-100") (util.tailwind-color "cr" "red-100") (util.tailwind-color "hc" "#fee2e2") (util.tailwind-color "rc" "rgb(254, 226, 226)") (util.pixel->rem 16) (util.rem->pixel 1) (util.hex->rgb "#222222") (util.rgb->hex "(34 34 34)") ]]
-return nil
--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -1,6 +1,6 @@
 -- [nfnl] Compiled from fnl/plugins/orgmode.fnl by https://github.com/Olical/nfnl, do not edit.
 local function _1_()
   local orgmode = require("orgmode")
-  return orgmode.setup({org_agenda_files = "~/orgfiles/*", org_default_notes_file = "~/orgfiles/tasks.org", org_todo_keywords = {"REPEAT", "TODO", "NEXT", "ACTIVE", "WAITING", "SOMEDAY", "PROJ", "|", "DONE", "CANCELLED"}})
+  return orgmode.setup({org_agenda_files = "~/orgfiles/*", org_default_notes_file = "~/orgfiles/tasks.org", org_todo_keywords = {"REPEAT", "TODO", "NEXT", "ACTIVE", "WAITING", "SOMEDAY", "PROJ", "|", "DONE", "CANCELLED", "COMPLETED"}})
 end
 return {{"nvim-orgmode/orgmode", event = "VeryLazy", ft = {"org"}, config = _1_}}
