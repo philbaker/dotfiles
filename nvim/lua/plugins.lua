912a7c873d75a64804a8607d0900a9b9c8e8d0d1
philbaker <philbaker@users.noreply.github.com>
2024-11-25 18:20:39 +0000

    fix nvim package issues

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,6 +4,16 @@ return {
     version = "*"
   },
   {
+    "folke/which-key.nvim",
+    version = "1.6.0",
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+  },
+  {
+    "terrortylor/nvim-comment",
+  },
+  {
     "Olical/nfnl",
     ft = "fennel",
     dependencies = { "norcalli/nvim.lua" },
