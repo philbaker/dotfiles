0a387e82f2bae28812d13ce610f017ed80c74c62
philbaker <philbaker@users.noreply.github.com>
2025-08-02 13:31:01 +0100

    add slimux

---
+++ nvim/fnl/plugins/slimux.fnl
@@ -0,0 +1,2 @@
+[{1 :esamattis/slimux
+  :config (fn [])}]
---
+++ nvim/lua/plugins/slimux.lua
@@ -0,0 +1,4 @@
+-- [nfnl] Compiled from fnl/plugins/slimux.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+end
+return {{"esamattis/slimux", config = _1_}}
