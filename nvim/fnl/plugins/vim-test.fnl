98870b01edaad49d2423a8c6678efa321daf1dd1
philbaker <philbaker@users.noreply.github.com>
2025-08-02 13:32:12 +0100

    Use slimux for vim-test

--- nvim/fnl/plugins/vim-test.fnl
+++ nvim/fnl/plugins/vim-test.fnl
@@ -3,6 +3,6 @@
 
 [{1 :vim-test/vim-test
   :config (fn []
-            (nvim.ex.let "test#strategy = 'neoterm'")
+            (nvim.ex.let "test#strategy = 'slimux'")
             (nvim.ex.let "test#php#phpunit#executable = 'vendor/bin/phpunit'")
             (nvim.ex.let "test#javascript#runner = 'vitest'"))}]
--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -3,7 +3,7 @@ local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local nvim = autoload("nvim")
 local function _2_()
-  nvim.ex.let("test#strategy = 'neoterm'")
+  nvim.ex.let("test#strategy = 'slimux'")
   nvim.ex.let("test#php#phpunit#executable = 'vendor/bin/phpunit'")
   return nvim.ex.let("test#javascript#runner = 'vitest'")
 end
