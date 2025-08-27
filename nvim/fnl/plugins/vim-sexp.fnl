82f099a88ece8b416f9f1c6472c05c29ebdd92b3
philbaker <philbaker@users.noreply.github.com>
2025-08-27 21:32:05 +0100

    Add vim-sexp

---
+++ nvim/fnl/plugins/vim-sexp.fnl
@@ -0,0 +1,3 @@
+[{1 :philbaker/vim-sexp
+  :dependencies [:tpope/vim-sexp-mappings-for-regular-people]
+  :init (fn [])}]
---
+++ nvim/lua/plugins/vim-sexp.lua
@@ -0,0 +1,4 @@
+-- [nfnl] fnl/plugins/vim-sexp.fnl
+local function _1_()
+end
+return {{"philbaker/vim-sexp", dependencies = {"tpope/vim-sexp-mappings-for-regular-people"}, init = _1_}}
