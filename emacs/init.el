841a9de4a4c3f2fb5cd18e202ce4cf59ed82e602
philbaker <philbaker@users.noreply.github.com>
2025-10-28 07:43:45 +0000

    Add example

--- emacs/init.el
+++ emacs/init.el
@@ -630,3 +630,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+; | TODO | IN-PROGRESS | DONE |
+; |------+------+------|
+; |      |             |      |
+; |      |             |      |
+; #+TBLFM: @1$1='(kanban-headers)::@2$1..@>$>='(kanban-zero @# $#)
